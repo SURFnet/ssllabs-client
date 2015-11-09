7fdb0de75fd9e91827f078683155ef477f7a1f48
Boy Baukema <boy@ibuildings.nl>
2015-11-09 13:16:52 +0100

    Add ValidateRequiredGradeService and update example accordingly.

--- examples/test.php
+++ examples/test.php
@@ -1,28 +1,14 @@
 <?php
-/**
- * Example of Client usage.
- *
- * Note that this requires that you install monolog/monolog as a logger implementation.
- */
 
 use Guzzle\Http\Client as HttpClient;
 use SURFnet\SslLabs\Client;
-use SURFnet\SslLabs\Dto\Host;
+use SURFnet\SslLabs\Dto\Endpoint;
+use SURFnet\SslLabs\Service\ValidateRequiredGradeService;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
-require __DIR__ . '/../../../../vendor/autoload.php';
-
-require __DIR__ . '/../src/Client.php';
-// Require all files in Dto/
-$dtoPath = __DIR__ . '/../src/Dto';
-foreach (scandir($dtoPath) as $filename) {
-    $path = $dtoPath . '/' . $filename;
-    if (is_file($path)) {
-        require $path;
-    }
-}
+require __DIR__ . '/../vendor/autoload.php';
 
 ini_set('xdebug.var_display_max_depth', 8);
 date_default_timezone_set('Europe/Amsterdam');
@@ -50,41 +36,17 @@ $info = $client->info();
 
 var_dump($info);
 
-var_dump($client->getStatusCodes());
+$hostname     = isset($argv[1]) ? $argv[1] : 'surfnet.nl';
+$passingGrade = isset($argv[2]) ? $argv[2] : Endpoint::GRADE_B;
 
-$host = $client->analyze(
-    'surfnet.nl',
-    false,
-    null,
-    null,
-    null,
-    Client::ALL_DONE
-);
+print "Starting analysis @ SSL Labs of $hostname" . PHP_EOL;
 
-var_dump($host);
+$validator = new ValidateRequiredGradeService($client);
+$validated = $validator->validateMatchesGrade($hostname, $passingGrade);
 
-
-function doNewAnalyze(Client $client)
-{
-    $host = $client->analyze(
-        'surfnet.nl',
-        false,
-        true,
-        false,
-        null,
-        Client::ALL_DONE
-    );
-
-    while (true) {
-        var_dump($host);
-
-        if (in_array($host->status, array(Host::STATUS_ERROR, Host::STATUS_READY))) {
-            exit;
-        }
-
-        sleep(10);
-        $host = $client->analyze('surfnet.nl');
-    }
+if ($validated) {
+    print "[PASS] SSL Labs gives $hostname an equal or greater grade than the required grade $passingGrade" . PHP_EOL;
 }
-
-
+else {
+    print "[FAIL] SSL Labs gives $hostname a grade less than the required grade $passingGrade" . PHP_EOL;
+}
---
+++ src/Service/ValidateRequiredGradeService.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace SURFnet\SslLabs\Service;
+
+use RuntimeException;
+use SURFnet\SslLabs\Client;
+use SURFnet\SslLabs\Dto\Host;
+
+class ValidateRequiredGradeService
+{
+  const SLEEP_TIME = 10;
+
+  /**
+   * @var Client
+   */
+  private $client;
+
+  /**
+   * @var int
+   */
+  private $maxAgeHours;
+
+  /**
+   * @var int
+   */
+  private $timeout;
+
+  /**
+   * @var array
+   */
+  private $resultsByHost = array();
+
+  /**
+   * ValidateRequiredGrade constructor.
+   * @param Client $client
+   * @param int $maxAgeHours
+   * @param int $timeout
+   */
+  public function __construct(
+    Client $client,
+    $maxAgeHours = 48,
+    $timeout = 300
+  ) {
+    $this->client = $client;
+    $this->maxAgeHours = $maxAgeHours;
+    $this->timeout = $timeout;
+  }
+
+  /**
+   * Synchronously (!) starts a new validation, waits for the results
+   * and compares the grade.
+   *
+   * This will take at least 60 seconds.
+   *
+   * @param string $hostName
+   * @param string $grade
+   * @return bool
+   */
+  public function validateMatchesGrade($hostName, $grade)
+  {
+    $hostDto = $this->analyze($hostName);
+
+    var_dump($hostDto);
+    foreach ($hostDto->endpoints as $endpointDto) {
+      if ($this->isGradeGreaterThan($endpointDto->grade, $grade)) {
+        continue;
+      }
+
+      return false;
+    }
+
+    return true;
+  }
+
+  private function analyze($hostName)
+  {
+    if (!empty($this->resultsByHost[$hostName])) {
+      return $this->resultsByHost[$hostName];
+    }
+
+    $remaining = $this->timeout;
+
+    $hostDto = $this->client->analyze($hostName, false, true);
+
+    while ($remaining > 0) {
+      $endStatuses = array(Host::STATUS_ERROR, Host::STATUS_READY);
+      if (in_array($hostDto->status, $endStatuses)) {
+        $this->resultsByHost[$hostName] = $hostDto;
+        return $this->resultsByHost[$hostName];
+      }
+
+      sleep(static::SLEEP_TIME);
+      $remaining -= static::SLEEP_TIME;
+
+      $hostDto = $this->client->analyze($hostName);
+    }
+
+    throw new RuntimeException(
+      "SSL Labs verfication exceeded max timeout of {$this->timeout} seconds"
+    );
+  }
+
+  /**
+   *
+   * @todo this doesn't work with A- or A+.
+   * @param $grade
+   * @param $passingGrade
+   * @return bool
+   */
+  private function isGradeGreaterThan($grade, $passingGrade)
+  {
+    return ord(substr($grade, 0, 1)) > ord(substr($passingGrade, 0, 1));
+  }
+}
